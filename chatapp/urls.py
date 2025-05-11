dd750eaea111c1b18bc69424a9795f6a0d84c2b8
mohammed99-m <markoriobrazil@gmail.com>
2025-05-11 20:37:05 +0300

    finish connect betwen servers

--- chatapp/urls.py
+++ chatapp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import SendMessageView
 
 urlpatterns = [
-    path('send/<str:user_id>/', SendMessageView, name='send_message'),
+    path('send/<int:user_id>/', SendMessageView, name='send_message'),
 ]

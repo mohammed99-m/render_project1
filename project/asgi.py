a81b0184430a20532158cd119125c013e6b96def
mohammed99-m <markoriobrazil@gmail.com>
2025-05-13 01:40:07 +0300

    add notification

--- project/asgi.py
+++ project/asgi.py
@@ -3,6 +3,7 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chatapp.routing
+import notification.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
@@ -10,7 +11,7 @@ application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            chatapp.routing.websocket_urlpatterns
+            chatapp.routing.websocket_urlpatterns +  notification.routing.websocket_urlpatterns
         )
     ),
 })
--- project/routing.py
+++ project/routing.py
@@ -1,11 +1,11 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chatapp.routing
-
+import notification.routing
 application = ProtocolTypeRouter({
     'websocket': AuthMiddlewareStack(
         URLRouter(
-            chatapp.routing.websocket_urlpatterns
+            chatapp.routing.websocket_urlpatterns +  notification.routing.websocket_urlpatterns
         )
     ),
 })

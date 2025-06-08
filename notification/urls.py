f8804384e58af8f1df1573ba1134cd03adf6fffd
mohammed99-m <markoriobrazil@gmail.com>
2025-06-09 02:51:26 +0300

    change in notification

--- notification/urls.py
+++ notification/urls.py
@@ -3,5 +3,5 @@ from .views import send_notification,send_notification2
 
 urlpatterns = [
     path('send-notifications/<str:user_id>/', send_notification, name='send_notification'),
-    path('send-notifications-push/<str:user_id>',send_notification2,name='send_notification2')
+    path('send-notifications-push/<str:user_id>/',send_notification2,name='send_notification2')
 ]

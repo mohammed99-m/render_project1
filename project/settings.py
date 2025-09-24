55e8075be17dc591905a5c7ed8c971c46f01d259
mohammed99-m <markoriobrazil@gmail.com>
2025-09-24 22:30:11 +0300

    edit

--- project/settings.py
+++ project/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'chatapp',
     'channels',
     'notification',
+    "corsheaders",
 
 ]
 
@@ -87,6 +88,7 @@ SIMPLE_JWT ={
 
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -96,6 +98,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
+
+CORS_ALLOW_CREDENTIALS = True 
 ROOT_URLCONF = 'project.urls'
 
 TEMPLATES = [
